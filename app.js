85497fd6442257e25045669cd3e845ff98d1666b
khalid225 <msart4real@yahoo.com>
2024-03-13 15:20:36 +0100

    added an scroll up button and made the navigation fixed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const menuIcon = document.querySelector(".menuIcon");
 const closeIcon = document.querySelector(".closeIcon");
 const dropDownMenu = document.querySelector(".dropdown_menu");
-// const arrowUp = document.querySelector("#arrowUp");
+const arrowUp = document.querySelector("#arrowUp");
 
 // toggle menu icon
 menuIcon.addEventListener("click", () => {
@@ -13,4 +13,22 @@ closeIcon.addEventListener("click", () => {
 	dropDownMenu.classList.toggle("open");
 	closeIcon.classList.toggle("hidden");
 	menuIcon.classList.toggle("hidden");
+});
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		arrowUp.style.display = "block";
+	} else {
+		arrowUp.style.display = "none";
+	}
+}
+
+arrowUp.addEventListener("click", () => {
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0;
 });

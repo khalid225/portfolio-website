347157307c8c6e2d6538ceb76b30198352713ab3
khalid225 <msart4real@yahoo.com>
2024-03-14 16:55:29 +0100

    Modified the hamburger menu and the links inside to scroll to the section when clicked and to also close the menu afterwards

--- app.js
+++ app.js
@@ -2,24 +2,16 @@ const menuIcon = document.querySelector(".menuIcon");
 const closeIcon = document.querySelector(".closeIcon");
 const dropDownMenu = document.querySelector(".dropdown_menu");
 const arrowUp = document.querySelector("#arrowUp");
+const links = document.querySelectorAll(".dropdown_menu li a");
 
 // toggle menu icon
-menuIcon.addEventListener("click", () => {
+function toggleMenu() {
 	dropDownMenu.classList.toggle("open");
 	closeIcon.classList.toggle("hidden");
 	menuIcon.classList.toggle("hidden");
-});
-closeIcon.addEventListener("click", () => {
-	dropDownMenu.classList.toggle("open");
-	closeIcon.classList.toggle("hidden");
-	menuIcon.classList.toggle("hidden");
-});
+}
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-	scrollFunction();
-};
-
+//checks if the user scrolls down 20px from the top of the document then show the button
 function scrollFunction() {
 	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
 		arrowUp.style.display = "block";
@@ -27,6 +19,29 @@ function scrollFunction() {
 		arrowUp.style.display = "none";
 	}
 }
+menuIcon.addEventListener("click", (event) => {
+	event.preventDefault();
+	toggleMenu();
+});
+closeIcon.addEventListener("click", (event) => {
+	event.preventDefault();
+	toggleMenu();
+});
+
+// Get all navigation links
+links.forEach((link) => {
+	link.addEventListener("click", function (event) {
+		event.preventDefault();
+		const targetSection = document.querySelector(this.getAttribute("href"));
+		targetSection.scrollIntoView({ behavior: "smooth" });
+		toggleMenu();
+	});
+});
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+	scrollFunction();
+};
 
 arrowUp.addEventListener("click", () => {
 	document.body.scrollTop = 0; // For Safari
